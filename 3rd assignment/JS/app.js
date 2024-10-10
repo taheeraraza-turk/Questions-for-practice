f3e5c4fe853d9fe6ed2586af9f50e7bd6695e62b
Taheera Raza Turk <taheeraraza0@gmail.com>
2024-10-10 07:23:14 -0700

    Add files via upload

---
+++ 3rd assignment/JS/app.js
@@ -0,0 +1,98 @@
+// 1.Write a javascript program that asks the user for their age and then displays a message indicating whether they are eligible to vote(18 or older).
+  
+            //  let age = prompt("What is your age?","Enter your age here..");
+            // if(age>=18){
+            //  document.write("You are eligible for voting✌..")
+            //  }
+            //  else if (age<18){
+            //   document.write("Sorry!😔..You are not eligible for voting..")
+            //   }
+            // else{
+            //   document.write("Error.. \n Please try again..")
+            // }
+       
+// 2.Use a conditional statement to determine whether a number is positive,negative,or zero.
+    
+          //   let number=prompt("Write a number to check whether it is positive or negative..","Enter ur number here..");
+          //   if(number<0){
+          //     document.write("The entered number is negative")
+          //   }
+          //   else if (number>0){
+          //     document.write("The entered number is positive")
+          //   }
+          //   else{
+          //     document.write("Invalid number! \n Please try again..")
+          //   }
+
+// 3.Write a javascript program that asks the user for their favorite color and then displays a message indicating whether they like a primary color (red,blue,or yellow,green).
+   
+            // let color = prompt("what is your favorite color?");
+            // if(color == "red" || color == "yellow"||color == "blue"  ){
+            //    document.write("The color you like is primary..🎨")
+            // }
+            // else if(color == "orange" ||color == "green" || color == "purple"  ){
+            //     document.write("The color you like is seconddary..🎨")
+            // }
+            // else {
+            //     document.write("The color you entered is not primary nor secondary..")
+            // }
+
+// 4.Use logical operators to determine whether a number is between 10 and 20 (use logical operator).
+           //  let num = prompt("  ","Enter a number here");
+
+           // if (num >10 && num<20){
+           //     document.write("The number you entered is between 10 and 20")
+           // }
+           // else if(num<=10 || num>=20){
+           //     document.write("The number you entered is not between 10 and 20")
+           // }
+           // else {
+           //     document.write("Invalid number... \n Try again")
+           // }
+// 5.Write a javascript program that asks the user for their grade (A,B,C,D or F) and then displays a message indicating whteher they passed or failed.
+ 
+           //   let grade = prompt("Enter your grade to check whether you passed or failed");
+
+           //   if(grade=="A" ||grade=="a"|| grade=="b" || grade=="B" || grade=="c" || grade=="C"){
+           //     document.write("You passed...!")
+           //   }
+           //   else if(grade=="D" || grade=="d" || grade=="f" || grade=="F"){
+           //     document.write("Unfortunately..You failed..!")
+           //   }
+           //   else{
+           //     document.write("Error.. \n Please try again")
+           //   }
+// 6.Write a javascript program that asks the user for their age,gender and then displays a message indicating whether they are eligible to serve in the military (18-30 years old,male).
+
+            //    let age=prompt("what is your age?");
+            //    let gender=prompt("What is your gender? \n write your gender in lowercase");
+
+            //    if(age<18 || gender == "female" || age>30){
+            //        document.write("Sorry..U are not eligible for this..")
+            //        }
+            //    else if(age>=18 && age<=30 && gender == "male"){
+            //        document.write("U are eligible for this..")
+            //        }
+            //    else{
+            //        document.write("Please try again")
+            //        }
+// 7.Use logical operatoprs to determine whether a number is even or odd.
+
+            // let numb = prompt("Enter a number to check whether it is even or odd");
+            // if ( numb%2 == 0 ){
+            //     document.write("The number you entered is even")
+            // }
+            // else if(numb%2 == 1){
+            //     document.write("The number you entered is odd")
+            // }
+            // else{
+            //     document.write("Try again");
+            // }
+// 8.Use conditional statement to determine whether a number is a multiple of 3. 
+            //  let number=prompt("Write a number");
+            //  if(number%3 == 0){
+            //     document.write("It is the multiple of 3")
+            //  }
+            //  else{
+            //     document.write("This number is not a multiple of 3")
+            //  }
